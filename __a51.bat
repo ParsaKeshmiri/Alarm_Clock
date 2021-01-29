7098e4ba0cc75547a36cf5303022a4084e29e848
Parsa Keshmiri <64231227+ParsaKeshmiri@users.noreply.github.com>
2021-01-28 20:50:34 -0800

    Delete __a51.bat

--- __a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\parsa\Desktop\ELEC 291\Lab_2\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\parsa\Desktop\ELEC 291\Lab_2\Alarm_Clock.asm"
-echo Crosside_Action Set_Hex_File C:\Users\parsa\Desktop\ELEC 291\Lab_2\Alarm_Clock.HEX
